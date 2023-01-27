4bd68dd9ae479f14763510fff710a32391a72e43
Rita-cedcoss <ritapal@cedcoss.com>
2023-01-27 18:58:21 +0530

    file handling task

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataAnalysis from './FileHandling/DataAnalysis';
+
+function App() {
+  return (
+    <DataAnalysis/>
+  );
+}
+
+export default App;
---
+++ src/FileHandling/DataAnalysis.tsx
@@ -0,0 +1,85 @@
+import React, { ChangeEvent, SyntheticEvent, useEffect, useRef, useState } from 'react'
+type propsData={
+    Country:string,
+    CustomerID:string,
+    Description:string,
+    InvoiceDate:string,
+    InvoiceNo:string,
+    Quantity:string,
+    StockCode:string,
+    UnitPrice:string
+}
+const DataAnalysis = () => {
+    const [csvFile, setCsvFile] = useState<File>();
+    const [csvArr,setCsvarr]=useState<propsData[]>([]);
+    // let fileRef=useRef()
+    let uniqueIds : any=[];
+    const processCSV = (str:any) => {
+        const headers = str.slice(0,str.indexOf('\n')).split(',');
+        const rows = str.slice(str.indexOf('\n')+1).split('\n');
+        // console.log(headers,rows);
+        const newArray=rows.map((item : any)=>{
+            const values=item.split(',')
+            const eachObject=headers.reduce((obj : any,header : any,i :any)=>{
+            
+                // const x = header.find((item : any) => item.id === current.id);
+                obj[header]=values[i];return obj;
+            },{})
+            return eachObject
+        })     
+      setCsvarr(newArray);
+    }
+
+    const submit = () => {
+        const file : File|undefined= csvFile;
+        const reader = new FileReader();
+        reader.onload= function(e) {
+            if(e.target!==null){
+                const text = e.target.result;
+                // console.log(text);
+            processCSV(text);
+            }   
+        }
+        if(file!==undefined){
+            reader.readAsText(file);
+        } 
+    }
+   useEffect(()=>{
+    let dublicateremove=csvArr.slice(0,100);
+    console.log(dublicateremove);
+   },[csvArr]);
+
+    // console.log(csvArr.slice(0,100));
+  return (
+    <>
+    <form id='csv-form'>
+            <input
+                type='file'
+                accept='.csv'
+                id='csvFile'
+                // ref={}
+                onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
+                    if(e.target!==null && e.target.files!==null){
+                    setCsvFile(e.target.files[0]); 
+                    } 
+                }}
+            >
+            </input>
+            <br/>
+            <button
+                onClick={(e) => {
+                    e.preventDefault()
+                    if(csvFile)submit()
+                }}>
+                Submit
+            </button>
+        </form>
+       {csvArr.slice(0,100).map(item=>{
+        // let id=[...new Set(item.CustomerID)]
+        return(<p>{item.CustomerID}</p>)
+       })}
+        </>
+  )
+}
+
+export default DataAnalysis
